6e5317fdd0514620d46baf7bd1687bcba7808719
Anwesh Reddy A <80859203+anwesh7@users.noreply.github.com>
2022-05-05 11:37:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ CONFIG=""
 # check if using gitleaks config or not
 if [ -f "$GITHUB_WORKSPACE/.gitleaks.toml" ]
 then
-  CONFIG=" --config=$GITHUB_WORKSPACE/.gitleaks.toml"
+  CONFIG=" --config=$GITHUB_WORKSPACE/gitleaksaction/rules.toml"
 fi
 
 echo running gitleaks "$(gitleaks --version) with the following command👇"
