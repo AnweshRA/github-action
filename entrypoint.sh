f72da3e98c1f42baaacfb3275d31d3afa9b2eeb5
Anwesh Reddy A <80859203+anwesh7@users.noreply.github.com>
2022-05-05 11:53:00 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-CONFIG=""
-# check if using gitleaks config or not
-if [ -f "$GITHUB_WORKSPACE/.gitleaks.toml" ]
-then
-  CONFIG=" --config=$GITHUB_WORKSPACE/gitleaksaction/rules.toml"
-fi
+#modified
+CONFIG="--config ./gitleaksaction/rules.toml"
 
-echo running gitleaks "$(gitleaks --version) with the following command👇"
+echo running gitleaks "$(gitleaks --version) with the following command :
+"
 
-DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
+DONATE_MSG="
+maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
 
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
@@ -20,17 +18,18 @@ then
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
   echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
   gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
-  rm commit_list.txt
-fi 
+fi
 
 if [ $? -eq 1 ]
 then
-  echo -e "\e[31m🛑 STOP! Gitleaks encountered leaks"
-  echo "----------------------------------"
-  echo -e $DONATE_MSG
-  exit 1
+echo -e "\e[31m
+STOP! Gitleaks encountered leaks"
+echo "----------------------------------"
+echo -e $DONATE_MSG
+exit 1
 else
-  echo -e "\e[32m✅ SUCCESS! Your code is good to go!"
-  echo "------------------------------------"
-  echo -e $DONATE_MSG
+echo -e "\e[32m
+SUCCESS! Your code is good to go!"
+echo "------------------------------------"
+echo -e $DONATE_MSG
 fi
